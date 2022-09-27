292deaf2f6c1992db04d0911df266a685974f4b6
vibovenkat123 <vibovenkat@gmail.com>
2022-09-27 18:22:35 -0500

    changed flexbox to grid

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,9 @@
 import CardItems from './components/card-items.vue';
 </script>
 <template>
-  <div class="flex flex-col items-center h-screen justify-center">
-    <CardItems />
+  <div class="flex justify-center">
+    <div class="flex flex-col items-center h-screen justify-center">
+      <CardItems />
+    </div>
   </div>
 </template>
