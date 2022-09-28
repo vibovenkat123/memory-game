c0c2229937e09413a2d386e6e9bb8f82523a6e5b
vibovenkat123 <vibovenkat@gmail.com>
2022-09-27 19:15:36 -0500

    fix username

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,5 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:vibovenkat/memory-game.git main:gh-pages
-
+git push -f git@github.com:vibovenkat123/memory-game.git main:gh-pages
 cd -
